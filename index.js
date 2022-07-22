c455f727df0c485a1570a55a0405c07eaf7dde09
Kent <kentaretorvik@gmail.com>
2022-07-22 21:37:54 +0200

    fix: out of bounds error in logic

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { adjektiv, substantiv, verb } = require('./words')
 
 function generateWord() {
-  let adj = adjektiv.words[Math.floor(Math.random() * adjektiv.length + 1)]
-  let sub = substantiv.words[Math.floor(Math.random() * substantiv.length + 1)]
-  let ver = verb.words[Math.floor(Math.random() * verb.length + 1)]
+  let adj = adjektiv.words[Math.floor(Math.random() * adjektiv.length)]
+  let sub = substantiv.words[Math.floor(Math.random() * substantiv.length)]
+  let ver = verb.words[Math.floor(Math.random() * verb.length)]
   return `${adj}-${sub}-${ver}`
 }
 
